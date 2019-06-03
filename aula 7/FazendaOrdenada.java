50e9eac85a7c406b0a4a3ba663a5dff76ff75f42
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-06-03 20:09:20 -0300

    Aula 7 add

---
+++ aula 7/FazendaOrdenada.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Gado{
+    public Gado(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    String nome;
+    int idade;
+}
+
+public class FazendaOrdenada{
+
+    public static void main(String[] args){
+        Gado[] boiada = new Gado[10];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 10; i++) {
+            String nome = sc.nextLine();
+            int idade = Integer.parseInt(sc.nextLine());
+
+            Gado boi = new Gado(nome, idade); 
+        }
+    }
+}
